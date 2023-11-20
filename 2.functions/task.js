237b39e280b2aa67a6ac33c4633adba35b4fbb42
Alexey Sokolov <dabero7111@gmail.com>
2023-11-20 22:39:46 +0300

    first homework

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,18 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+	let min, max, sum, avg;
+	min = arr[0];
+	max = min;
+	sum = 0;
+	n = 0;
+	for (i=0; i < arr.length ; ++i){
+	  if (arr[i] > max) max = arr[i];
+	  if (arr[i] < min) min = arr[i];
+	  sum += arr[i];
+	  n++;
+	}
+	avg = Number((sum/n).toFixed(2));
+	
+	return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
