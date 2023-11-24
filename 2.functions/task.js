b361d917a602e01fc13d7307ee4aaad4e2041f8a
Alexey Sokolov <dabero7111@gmail.com>
2023-11-24 18:39:53 +0300

    initial commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,19 +1,21 @@
 function getArrayParams(...arr) {
-	let min, max, sum;
-	min = Infinity;
-	max = -Infinity;
-	sum = 0;
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+
 	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
 		if (arr[i] > max) {
-		 max = arr[i];
-	  }else if (arr[i] < min) {
-		 min = arr[i];
-	  }
-	  sum += arr[i];
+			max = arr[i];
+		}
+		sum += arr[i];
 	}
+
 	const avg = +(sum / arr.length).toFixed(2);
 	return { min: min, max: max, avg: avg };
- }
+}
 
 function summElementsWorker(...arr) {
 
