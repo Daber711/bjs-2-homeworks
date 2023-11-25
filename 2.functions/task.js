f58418bb8415589eea63a40e1eedc23b4b6215ef
Alexey Sokolov <dabero7111@gmail.com>
2023-11-25 12:22:34 +0300

    function commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -34,22 +34,19 @@ function differenceMaxMinWorker(...arr) {
 
 function differenceEvenOddWorker(...arr) {
 	if (!arr || arr.length === 0) {
-    return 0;
-  }
-  
-  let sumEvenElement = 0;
-  let sumOddElement = 0;
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      sumEvenElement += arr[i];
-    } else {
-      sumOddElement += arr[i];
-    }
-  }
-  
-  return sumEvenElement - sumOddElement;
-}
+	  return 0;
+	}
+	  let sumEvenElement = 0;
+	  let sumOddElement = 0;
+	for (let i = 0; i < arr.length; i++) {
+	if (arr[i] % 2 === 0) {
+	  sumEvenElement += arr[i];
+	} else {
+	sumOddElement += arr[i];
+	 }
+	}
+	 return sumOddElement - sumEvenElement;
+ }
 
 function averageEvenElementsWorker(...arr) {
    let sumEvenElement = 0;
